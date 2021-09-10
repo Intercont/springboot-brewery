578ce87f8c497b341cdb9a6e78a10a2b080f06dc
Igor Fraga <igoravancinifraga@gmail.com>
2021-09-10 13:04:47 -0300

    Initial commit

---
+++ src/test/java/com/igorfraga/brewery/SpringbootBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorfraga.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
